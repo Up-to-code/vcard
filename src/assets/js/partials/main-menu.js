dfd0b49dbf1e3267467cd5adb4b03c8d0cc69ecd
Up-to-code <133324agh@gmail.com>
2024-10-27 03:13:59 +0300

    tyr

--- src/assets/js/partials/main-menu.js
+++ src/assets/js/partials/main-menu.js
@@ -7,72 +7,48 @@ class NavigationMenu extends HTMLElement {
                 this.displayAllText = salla.lang.get('blocks.home.display_all');
 
                 return salla.api.component.getMenus()
-                    .then(({ data }) => {
-                        this.menus = data;
-                        this.render();
-                        this.addToggleEvents();
-                    }).catch((error) => salla.logger.error('salla-menu::Error fetching menus', error));
+                .then(({ data }) => {
+                    this.menus = data;
+                    return this.render()
+                }).catch((error) => salla.logger.error('salla-menu::Error fetching menus', error));
             });
     }
 
-    addToggleEvents() {
-        const menuItems = this.querySelectorAll('.root-level');
-
-        menuItems.forEach(item => {
-            const link = item.querySelector('a');
-            const subMenu = item.querySelector('.sub-menu');
-            
-            // Check if the menu item has children
-            if (subMenu) {
-                link.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.toggleMenu(item);
-                });
-                
-                // Close menu when clicking outside
-                document.addEventListener('click', (e) => {
-                    if (!item.contains(e.target)) {
-                        item.classList.remove('open');
-                    }
-                });
-            }
-        });
-
-        const mobileMenuButton = this.querySelector('.close-mobile-menu');
-        mobileMenuButton.addEventListener('click', () => {
-            this.toggleMobileMenu();
-        });
-    }
-
-    toggleMenu(item) {
-        const allItems = this.querySelectorAll('.root-level');
-
-        allItems.forEach(otherItem => {
-            if (otherItem !== item) {
-                otherItem.classList.remove('open');
-            }
-        });
-
-        item.classList.toggle('open');
-    }
-
-    toggleMobileMenu() {
-        const mobileMenu = this.querySelector('#mobile-menu');
-        mobileMenu.classList.toggle('open');
-    }
-
+    /** 
+    * Check if the menu has children
+    * @param {Object} menu
+    * @returns {Boolean}
+    */
     hasChildren(menu) {
         return menu?.children?.length > 0;
     }
 
+    /**
+    * Check if the menu has products
+    * @param {Object} menu
+    * @returns {Boolean}
+    */
     hasProducts(menu) {
         return menu?.products?.length > 0;
     }
 
+    /**
+    * Get the classes for desktop menu
+    * @param {Object} menu
+    * @param {Boolean} isRootMenu
+    * @returns {String}
+    */
     getDesktopClasses(menu, isRootMenu) {
-        return `${isRootMenu ? 'root-level' : 'relative'} ${menu.products ? 'mega-menu' : ''} ${this.hasChildren(menu) ? 'has-children' : ''}`;
+        return `!hidden lg:!block ${isRootMenu ? 'root-level lg:!inline-block' : 'relative'} ${menu.products ? ' mega-menu' : ''}
+        ${this.hasChildren(menu) ? ' has-children' : ''}`
     }
 
+    /**
+    * Get the mobile menu
+    * @param {Object} menu
+    * @param {String} displayAllText
+    * @returns {String}
+    */
     getMobileMenu(menu, displayAllText) {
         const menuImage = menu.image ? `<img src="${menu.image}" class="rounded-full" width="48" height="48" alt="${menu.title}" />` : '';
 
@@ -82,7 +58,8 @@ class NavigationMenu extends HTMLElement {
                 <a href="${menu.url}" aria-label="${menu.title || 'category'}" class="text-gray-500 ${menu.image ? '!py-3' : ''}" ${menu.link_attrs}>
                     ${menuImage}
                     <span>${menu.title || ''}</span>
-                </a>` : `
+                </a>` :
+                `
                 <span class="${menu.image ? '!py-3' : ''}">
                     ${menuImage}
                     ${menu.title}
@@ -92,16 +69,22 @@ class NavigationMenu extends HTMLElement {
                         <a href="${menu.url}" class="text-gray-500">${displayAllText}</a>
                     </li>
                     ${menu.children.map((subMenu) => this.getMobileMenu(subMenu, displayAllText)).join('')}
-                </ul>`}
+                </ul>
+            `}
         </li>`;
     }
 
+    /**
+    * Get the desktop menu
+    * @param {Object} menu
+    * @param {Boolean} isRootMenu
+    * @returns {String}
+    */
     getDesktopMenu(menu, isRootMenu) {
         return `
         <li class="${this.getDesktopClasses(menu, isRootMenu)}" ${menu.attrs}>
             <a href="${menu.url}" aria-label="${menu.title || 'category'}" ${menu.link_attrs}>
                 <span>${menu.title}</span>
-                ${this.hasChildren(menu) ? '<span class="menu-arrow">▼</span>' : ''}
             </a>
             ${this.hasChildren(menu) ? `
                 <div class="sub-menu ${this.hasProducts(menu) ? 'w-full left-0 flex' : 'w-56'}">
@@ -109,11 +92,18 @@ class NavigationMenu extends HTMLElement {
                         ${menu.children.map((subMenu) => this.getDesktopMenu(subMenu, false)).join('\n')}
                     </ul>
                     ${this.hasProducts(menu) ? `
-                    <salla-products-list source="selected" shadow-on-hover source-value="[${menu.products}]" />` : ''}
+                    <salla-products-list
+                    source="selected"
+                    shadow-on-hover
+                    source-value="[${menu.products}]" />` : ''}
                 </div>` : ''}
         </li>`;
     }
 
+    /**
+    * Get the menus
+    * @returns {String}
+    */
     getMenus() {
         return this.menus.map((menu) => `
             ${this.getMobileMenu(menu, this.displayAllText)}
@@ -121,8 +111,11 @@ class NavigationMenu extends HTMLElement {
         `).join('\n');
     }
 
+    /**
+    * Render the header menu
+    */
     render() {
-        this.innerHTML = `
+        this.innerHTML =  `
         <nav id="mobile-menu" class="mobile-menu">
             <ul class="main-menu">${this.getMenus()}</ul>
             <button class="btn--close close-mobile-menu sicon-cancel lg:hidden"></button>
